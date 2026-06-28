a6112a90f10b32ff4c6a678dcdfd37889eb0f160
precious654 <odukwechiemeka@gmail.com>
2026-06-28 02:45:24 -0400

    users can now sign in

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -7,68 +7,150 @@ import { useAuth } from "@/context/AuthContext";
 type step = "signup" | "signin";
 
 const AuthForm = () => {
+  const { session, signInUser } = useAuth();
   const [step, setStep] = React.useState<step>("signup");
 
-  const { session } = useAuth();
-  console.log(session);
+  const [error, submitAction, isPending] = React.useActionState(
+    async (prevState, formData: FormData) => {
+      let userData;
+      const confirmPassword = formData.get("confirmPassword");
+
+      if (step === "signup") {
+        userData = {
+          name: formData.get("name"),
+          email: formData.get("email"),
+          password: formData.get("password"),
+        };
+
+        if (userData.password !== confirmPassword) {
+          return new Error("Passwords do not match");
+        }
+
+        // const{success, data, error: signUpError} = await signUp();
+
+        // if(signInError) {
+        //   return new Error(signUpError);
+        // }
+
+        // if(success && data.session) {
+        //   return null;
+        // }
+
+        return null;
+      } else if (step === "signin") {
+        userData = {
+          email: formData.get("email"),
+          password: formData.get("password"),
+        };
+
+        const {
+          success,
+          data,
+          error: signInError,
+        } = await signInUser(userData.email, userData.password);
+
+        if (signInError) {
+          return new Error(signInError);
+        }
+
+        if (success && data.session) {
+          return null;
+        }
+
+        return null;
+      }
+      console.log(userData);
+    },
+    null
+  );
 
   return (
     <>
-      {step === "signup" && (
-        <form className="flex flex-col gap-4 w-[50%] mx-auto mt-10">
-          <input
-            type="text"
-            className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
-            placeholder="Your username"
-            name="name"
-            id="name"
-          />
-          <input
-            type="email"
-            className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
-            placeholder="Your email"
-            name="email"
-            id="email"
-          />
-          <input
-            type="password"
-            className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
-            placeholder="Password"
-            name="password"
-            id="password"
-          />
-          <input
-            type="password"
-            className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
-            placeholder="Confirm password"
-            name="confirmPassword"
-            id="confirmPassword"
-          />
-          <button className="py-4 rounded-full bg-amber-400 w-full mt-3 text-white">
-            sign up
-          </button>
-        </form>
-      )}
+      <form
+        action={submitAction}
+        className="flex flex-col gap-4 w-[50%] mx-auto mt-10"
+      >
+        {step === "signup" && (
+          <>
+            <input
+              type="text"
+              className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
+              placeholder="Your username"
+              name="name"
+              id="name"
+            />
+            <input
+              type="email"
+              className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
+              placeholder="Your email"
+              name="email"
+              id="email"
+            />
+            <input
+              type="password"
+              className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
+              placeholder="Password"
+              name="password"
+              id="password"
+            />
+            <input
+              type="password"
+              className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
+              placeholder="Confirm password"
+              name="confirmPassword"
+              id="confirmPassword"
+            />
+            <button className="py-4 rounded-full bg-amber-400 w-full mt-3 text-white">
+              {isPending ? "signing up" : "sign up"}
+            </button>
+            <p className="text-sm">
+              Already have an account?{" "}
+              <button
+                className="text-sm font-semibold underline cursor-pointer"
+                onClick={() => setStep("signin")}
+              >
+                sign in
+              </button>
+            </p>
+          </>
+        )}
 
-      {step === "signin" && (
-        <form className="flex flex-col gap-4 w-[50%] mx-auto mt-10">
-          <input
-            type="email"
-            className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
-            name="email"
-            id="email"
-          />
-          <input
-            type="password"
-            className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
-            name="password"
-            id="password"
-          />
-          <button className="py-4 rounded-full bg-amber-400 w-full mt-3 text-white">
-            sign in
-          </button>
-        </form>
-      )}
+        {step === "signin" && (
+          <>
+            <input
+              type="email"
+              className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
+              placeholder="Your email"
+              name="email"
+              id="email"
+            />
+            <input
+              type="password"
+              className="rounded-2xl py-4 px-3 bg-gray-300 outline-0"
+              placeholder="Password"
+              name="password"
+              id="password"
+            />
+            <button className="py-4 rounded-full bg-amber-400 w-full mt-3 text-white">
+              {isPending ? "signing in" : "sign in"}
+            </button>
+            <p className="text-sm">
+              Already have an account?{" "}
+              <button
+                className="text-sm font-semibold underline cursor-pointer"
+                onClick={() => setStep("signup")}
+              >
+                sign up
+              </button>
+            </p>
+          </>
+        )}
+        {error && (
+          <div role="alert" className="mt-4 text-sm text-red-500 font-medium">
+            {error.message}
+          </div>
+        )}
+      </form>
     </>
   );
 };
--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 
+import { createClient } from "@/libs/supabase/client";
+
 const AuthContext = React.createContext();
 
 export const AuthContextProvider = ({
@@ -9,13 +11,53 @@ export const AuthContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [session, setSession] = React.useState(undefined);
+  const supabase = createClient();
+  const [session, setSession] = React.useState<any>(undefined);
+
+  React.useEffect(() => {
+    const getInitialSession = async () => {
+      try {
+        const { data, error } = await supabase.auth.getSession();
+        if (error) {
+          throw error;
+        }
+        setSession(data.session);
+      } catch (error) {
+        console.error("Error getting session", error.message);
+      }
+    };
+    getInitialSession();
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      console.log("Session changed", session);
+    });
+  }, [supabase]);
+
+  const signInUser = async (email: string, password: string) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email.toLowerCase(),
+        password: password,
+      });
+      if (error) {
+        console.error("Supabase sign-in error: ", error.message);
+        return { success: false, error: error.message };
+      }
+      console.log("Sign in was a success: ", data);
+      return { success: true, data: data };
+
+    } catch (error) {
+      console.error("An unexpected sign in error: ", error.message);
+      return { success: false, error: "An unexpected error occurred. Please try again." };
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, signInUser }}>{children}</AuthContext.Provider>
   );
 };
 
 export const useAuth = () => {
-	return React.useContext(AuthContext);
-}
+  return React.useContext(AuthContext);
+};
